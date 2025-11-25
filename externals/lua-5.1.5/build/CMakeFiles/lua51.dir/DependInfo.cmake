07ad68610fb7af483303cf78e6b4ef8af45628bd
rick <rohit1322saini@gmail.com>
2025-11-25 21:49:14 +0530

    TODO: create lua repo

---
+++ externals/lua-5.1.5/build/CMakeFiles/lua51.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lapi.c" "CMakeFiles/lua51.dir/src/lapi.c.o" "gcc" "CMakeFiles/lua51.dir/src/lapi.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lauxlib.c" "CMakeFiles/lua51.dir/src/lauxlib.c.o" "gcc" "CMakeFiles/lua51.dir/src/lauxlib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lbaselib.c" "CMakeFiles/lua51.dir/src/lbaselib.c.o" "gcc" "CMakeFiles/lua51.dir/src/lbaselib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lcode.c" "CMakeFiles/lua51.dir/src/lcode.c.o" "gcc" "CMakeFiles/lua51.dir/src/lcode.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ldblib.c" "CMakeFiles/lua51.dir/src/ldblib.c.o" "gcc" "CMakeFiles/lua51.dir/src/ldblib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ldebug.c" "CMakeFiles/lua51.dir/src/ldebug.c.o" "gcc" "CMakeFiles/lua51.dir/src/ldebug.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ldo.c" "CMakeFiles/lua51.dir/src/ldo.c.o" "gcc" "CMakeFiles/lua51.dir/src/ldo.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ldump.c" "CMakeFiles/lua51.dir/src/ldump.c.o" "gcc" "CMakeFiles/lua51.dir/src/ldump.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lfunc.c" "CMakeFiles/lua51.dir/src/lfunc.c.o" "gcc" "CMakeFiles/lua51.dir/src/lfunc.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lgc.c" "CMakeFiles/lua51.dir/src/lgc.c.o" "gcc" "CMakeFiles/lua51.dir/src/lgc.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/linit.c" "CMakeFiles/lua51.dir/src/linit.c.o" "gcc" "CMakeFiles/lua51.dir/src/linit.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/liolib.c" "CMakeFiles/lua51.dir/src/liolib.c.o" "gcc" "CMakeFiles/lua51.dir/src/liolib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/llex.c" "CMakeFiles/lua51.dir/src/llex.c.o" "gcc" "CMakeFiles/lua51.dir/src/llex.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lmathlib.c" "CMakeFiles/lua51.dir/src/lmathlib.c.o" "gcc" "CMakeFiles/lua51.dir/src/lmathlib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lmem.c" "CMakeFiles/lua51.dir/src/lmem.c.o" "gcc" "CMakeFiles/lua51.dir/src/lmem.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/loadlib.c" "CMakeFiles/lua51.dir/src/loadlib.c.o" "gcc" "CMakeFiles/lua51.dir/src/loadlib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lobject.c" "CMakeFiles/lua51.dir/src/lobject.c.o" "gcc" "CMakeFiles/lua51.dir/src/lobject.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lopcodes.c" "CMakeFiles/lua51.dir/src/lopcodes.c.o" "gcc" "CMakeFiles/lua51.dir/src/lopcodes.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/loslib.c" "CMakeFiles/lua51.dir/src/loslib.c.o" "gcc" "CMakeFiles/lua51.dir/src/loslib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lparser.c" "CMakeFiles/lua51.dir/src/lparser.c.o" "gcc" "CMakeFiles/lua51.dir/src/lparser.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lstate.c" "CMakeFiles/lua51.dir/src/lstate.c.o" "gcc" "CMakeFiles/lua51.dir/src/lstate.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lstring.c" "CMakeFiles/lua51.dir/src/lstring.c.o" "gcc" "CMakeFiles/lua51.dir/src/lstring.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lstrlib.c" "CMakeFiles/lua51.dir/src/lstrlib.c.o" "gcc" "CMakeFiles/lua51.dir/src/lstrlib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ltable.c" "CMakeFiles/lua51.dir/src/ltable.c.o" "gcc" "CMakeFiles/lua51.dir/src/ltable.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ltablib.c" "CMakeFiles/lua51.dir/src/ltablib.c.o" "gcc" "CMakeFiles/lua51.dir/src/ltablib.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/ltm.c" "CMakeFiles/lua51.dir/src/ltm.c.o" "gcc" "CMakeFiles/lua51.dir/src/ltm.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lua.c" "CMakeFiles/lua51.dir/src/lua.c.o" "gcc" "CMakeFiles/lua51.dir/src/lua.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/luac.c" "CMakeFiles/lua51.dir/src/luac.c.o" "gcc" "CMakeFiles/lua51.dir/src/luac.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lundump.c" "CMakeFiles/lua51.dir/src/lundump.c.o" "gcc" "CMakeFiles/lua51.dir/src/lundump.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lvm.c" "CMakeFiles/lua51.dir/src/lvm.c.o" "gcc" "CMakeFiles/lua51.dir/src/lvm.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lzio.c" "CMakeFiles/lua51.dir/src/lzio.c.o" "gcc" "CMakeFiles/lua51.dir/src/lzio.c.o.d"
+  "/run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/print.c" "CMakeFiles/lua51.dir/src/print.c.o" "gcc" "CMakeFiles/lua51.dir/src/print.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ externals/lua-5.1.5/build/CMakeFiles/lua51.dir/src/linit.c.o.d
@@ -0,0 +1,28 @@
+CMakeFiles/lua51.dir/src/linit.c.o: \
+ /run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/linit.c \
+ /usr/include/stdc-predef.h \
+ /run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lua.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stddef.h \
+ /run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/luaconf.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/bits/pthread_stack_min.h /usr/include/bits/posix2_lim.h \
+ /run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lualib.h \
+ /run/media/rick/8b5b9054-ef1c-4785-aa10-f6a2608b67c8/ArchLinux/work/raylib-cpp/rohit/externals/lua-5.1.5/src/lauxlib.h \
+ /usr/include/stdio.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h
